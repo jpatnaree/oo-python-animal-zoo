98e6ae9f01dc564cb56d7dd23e8404a9b85a44ea
Patnaree Johnson <j.patnaree.t@gmail.com>
2023-11-18 11:58:23 -0500

    practice

--- lib/animal.py
+++ lib/animal.py
@@ -1,2 +1,25 @@
 class Animal:
-    pass
+
+    all = []
+
+    def __init__(self, species, weight, nickname, zoo):
+        self.species = species
+        self.weight = weight
+        self.nickname = nickname
+        self.zoo = zoo
+        Animal.all.append(self)
+
+    @property
+    def weight(self):
+        return self._weight
+    
+    @weight.setter
+    def weight(self,new_weight):
+        if isinstance(new_weight, (float or int)):
+            self._weight = new_weight
+        else:
+            raise Exception('')
+
+    @classmethod
+    def find_by_species(cls, species):
+        return [a for a in cls.all if a.species == species]
--- lib/zoo.py
+++ lib/zoo.py
@@ -1,4 +1,45 @@
 from lib.animal import Animal
 
 class Zoo:
-    pass
+
+    all = []
+    
+    def __init__(self, name, location):
+        self.name = name
+        self.location = location
+        Zoo.all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, new_name):
+        if isinstance(new_name, str):
+            self._name = new_name
+        else:
+            raise Exception('Name must be a string')
+        
+    @property
+    def location(self):
+        return self._location
+    
+    @location.setter
+    def location(self, new_lo):
+        if isinstance(new_lo, str):
+            self._location = new_lo
+        else:
+            raise Exception('Location must be a string')
+
+    def animal_species(self):
+        return [a.species for a in Animal.all]
+    
+    def find_by_species(self, species):
+        return [a for a in Animal.all if a.zoo == self and a.species == species]
+
+    def animal_nicknames(self):
+        return [a.nickname for a in Animal.all if a.zoo == self]
+    
+    @classmethod
+    def find_by_location(cls, location):
+        return [z for z in Zoo.all if z.location == location]
